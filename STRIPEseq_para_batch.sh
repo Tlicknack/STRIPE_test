9d015bf4957ca2c1aca8ecd9e968d3b4cac656d2
Timothy Licknack <tlicknac@h2.carbonate.uits.iu.edu>
2018-07-02 22:07:48 -0400

    Final draft, is working for Pnov

--- STRIPEseq_para_batch.sh
+++ STRIPEseq_para_batch.sh
@@ -3,7 +3,7 @@
 #PBS -N STRIPEseq_para_test
 #PBS -k o
 #PBS -l nodes=1:ppn=8,vmem=48gb
-#PBS -l walltime=3:00:00
+#PBS -l walltime=5:00:00
 
 module load java
 module load samtools
@@ -12,7 +12,7 @@ module load trimmomatic
 module unload python
 module load python/3.6.1 
 
-WD=/N/u/tlicknac/Carbonate/STRIPE_Novaurelia/
+WD=/N/dc2/scratch/tlicknac/STRIPE_Novaurelia/
 
 cd $WD
 
